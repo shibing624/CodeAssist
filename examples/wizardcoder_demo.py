ec532fb44d8c64f54bd62fdbf8f8da3e988e0b42
shibing624 <shibing624@126.com>
2023-06-30 17:31:47 +0800

    update inference_demo.py

--- examples/wizardcoder_demo.py
+++ examples/wizardcoder_demo.py
@@ -12,11 +12,8 @@ m = WizardCoder("WizardLM/WizardCoder-15B-V1.0")
 print(m.generate('def load_csv_file(file_path):')[0])
 
 prompts = [
-    "write a function to load csv file",
     "write a function to load csv file and return a dataframe",
-    "sort a list of integers",
     "give me odd numbers from 1 to 10",
-    "write a function to show fibonacci sequence",
     "write a function to show fibonacci sequence and return a list",
     "write a Java code to sum 1 to 10.",
     "write a python code to sum 1 to 10.",
